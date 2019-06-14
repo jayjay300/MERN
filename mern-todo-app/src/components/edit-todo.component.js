f47788698e0e895cfc8b57275a415c5d250e43bc
jayjay300 <mulcahyjohn8@gmail.com>
2019-06-14 17:36:28 -0400

    submodule fix

---
+++ mern-todo-app/src/components/edit-todo.component.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class CreateTodo extends Component{
+	render(){
+
+		return(
+
+			<div>
+				<p>Welcome to Edit Todo Component!!</p>
+
+
+			</div>
+
+
+
+
+
+			)
+
+
+
+
+	}
+
+
+
+
+}
